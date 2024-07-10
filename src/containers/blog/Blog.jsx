e4bea6df1f8aaefec9446c2dfdbad3306508372e
shivakumar2006 <official.shivakumar06@gmail.com>
2024-07-10 15:56:35 +0530

    adding changes

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 const Blog = () => {
@@ -11,9 +12,34 @@ const Blog = () => {
         </h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container_groupA">Article</div>
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="jul-10, 2024"
+            title="GPT-3 and Open AI is the future. Lut us explore how it is."
+          />
+        </div>
         <div className="gpt3__blog-container_groupB">
-          Article Article Article Article
+          <Article
+            imgUrl={blog02}
+            date="jul-10, 2024"
+            title="GPT-3 and Open AI is the future. Lut us explore how it is."
+          />
+          <Article
+            imgUrl={blog03}
+            date="jul-10, 2024"
+            title="GPT-3 and Open AI is the future. Lut us explore how it is."
+          />
+          <Article
+            imgUrl={blog04}
+            date="jul-10, 2024"
+            title="GPT-3 and Open AI is the future. Lut us explore how it is."
+          />
+          <Article
+            imgUrl={blog05}
+            date="jul-10, 2024"
+            title="GPT-3 and Open AI is the future. Lut us explore how it is."
+          />
         </div>
       </div>
     </div>
